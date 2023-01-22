d9c837552849da2fc3f55d9863b28916834a4023
Emre Yıldız <emreyildiz183@gmail.com>
2023-01-22 13:08:29 +0300

    taş konulma eklendi

--- main.py
+++ main.py
@@ -18,13 +18,21 @@ def sablon():
 
 def tas_kordinat_iste():
     if oyun_sira[0] % 2 == 0:
-        tas_kordinat = input("Lütfen X koymak istediğiniz yerin kordinatlarını yazınız")
+        tas_kordinat = input("Lütfen X koymak istediğiniz yerin kordinatlarını yazınız ")
     else:
-        tas_kordinat = input("Lütfen O koymak istediğiniz yerin kordinatlarını yazınız")
+        tas_kordinat = input("Lütfen O koymak istediğiniz yerin kordinatlarını yazınız ")
     while tas_kordinat[0] not in ["1","2","3"] and tas_kordinat[1].upper() not in x_indexleri:
-        tas_kordinat = input("Tahtada olmayan bir konum girdiniz lütfen tekrar yazınız")
+        tas_kordinat = input("Tahtada olmayan bir konum girdiniz lütfen tekrar yazınız ")
+    while kordinat_sistemi[int(tas_kordinat[0])][x_eksenleri[tas_kordinat[1]]] != "   ":
+        tas_kordinat = input("İlgili konumda başka bir taş var lütfen tekrar giriniz ")
 
     return tas_kordinat
 
-
+def tas_koy(tas_kordinat):
+    if oyun_sira[0] % 2 == 0:
+        kordinat_sistemi[int(tas_kordinat[0])-1][x_eksenleri[tas_kordinat[1]]] = "X"
+    else:
+        kordinat_sistemi[int(tas_kordinat[0])-1][x_eksenleri[tas_kordinat[1]]] = "O"
+    oyun_sira[0] += 1
+    
 
